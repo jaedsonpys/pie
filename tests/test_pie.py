09064874113c4c45ad12e5cd431586de3096d976
Jaedson Silva <contacanal798@gmail.com>
2022-09-03 15:55:30 -0300

    adding test of _index_file_lines method

--- tests/test_pie.py
+++ tests/test_pie.py
@@ -27,7 +27,7 @@ class TestPie(bupytest.UnitTest):
             writer.write('Hello world!')
 
         with open('tests/pie-test/02.txt', 'w') as writer:
-            writer.write('Good morning.')
+            writer.write('Good morning.\n\nHow are you?')
 
     def test_create_repository(self):
         author = 'Jaedson'
@@ -65,6 +65,24 @@ class TestPie(bupytest.UnitTest):
             expected={
                 'filename': 'tests/pie-test/02.txt',
                 'hash': self.pie._get_file_hash('tests/pie-test/02.txt')
+            }
+        )
+
+    def test_index_file_lines(self):
+        file_lines_1 = self.pie._index_file_lines('tests/pie-test/01.txt')
+        file_lines_2 = self.pie._index_file_lines('tests/pie-test/02.txt')
+
+        self.assert_expected(
+            value=file_lines_1,
+            expected={0: 'Hello world!'}
+        )
+
+        self.assert_expected(
+            value=file_lines_2,
+            expected={
+                0: 'Good morning.',
+                1: '',
+                2: 'How are you?'
             }
         )
 
