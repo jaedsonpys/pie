a691bf9c9ceee51db130c32bfe641007ef531a0d
Jaedson Silva <contacanal798@gmail.com>
2022-09-03 01:17:35 -0300

    creating Pie class and adding method to create a repository

---
+++ pie/pie.py
@@ -0,0 +1,30 @@
+import json
+
+import os
+import secrets
+import hashlib
+
+from . import exceptions
+
+
+class Pie(object):
+    def create_repository(self, author: str, author_email: str) -> None:
+        repository_path = os.path.join(os.getcwd(), '.pie')
+
+        if os.path.isdir(repository_path):
+            raise exceptions.RepositoryExistsError(f'Repository exists in "{repository_path}"')
+
+        os.mkdir(repository_path)
+        key = hashlib.sha256(secrets.token_bytes(32)).hexdigest()
+
+        repository_info = {
+            'author': author,
+            'author_email': author_email,
+            'key': key,
+            'remote': None
+        }
+
+        repository_info_file = os.path.join(repository_path, '.info')
+
+        with open(repository_info_file, 'w') as writer:
+            json.dump(repository_info, writer, indent=4)
