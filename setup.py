d8952e156e9a9845f68341e1edde1369e0d8e791
Jaedson Silva <contacanal798@gmail.com>
2022-09-04 22:12:45 -0300

    creating setup script

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+from pie import __version__
+
+with open('README.md', 'r') as reader:
+    readme = reader.read()
+
+setup(
+    author='Jaedson Silva',
+    author_email='imunknowuser@protonmail.com',
+    name='pie',
+    description='Pie, a complete version control.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    url='https://github.com/jaedsonpys/pie',
+    license='GPL v3.0',
+    version=__version__,
+    packages=['pie'],
+    python_requires='>=3.7',
+    install_requires=['utokeniz'],
+    keywords=['control', 'version', 'versioning', 'file']
+)
