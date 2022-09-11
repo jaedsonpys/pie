65d7c6b8a369c16fdf67c348c4c0e9f663e4c879
Jaedson Silva <contacanal798@gmail.com>
2022-09-10 21:55:13 -0300

    import Pie class and exceptions in __init__.py

--- pie/__init__.py
+++ pie/__init__.py
@@ -14,4 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+from .pie import Pie
+from . import exceptions
+
 __version__ = '1.0.0'
