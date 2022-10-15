b306f51f82107cda86a9d19a84728d40a60cf9e4
Jaedson Silva <contacanal798@gmail.com>
2022-10-15 18:40:43 -0300

    using load_hooks as private method

--- pie/hooks.py
+++ pie/hooks.py
@@ -16,16 +16,14 @@ class Hooks(object):
         """
 
         self._hooks_filepath = hooks_filepath
-        self.hooks = {}
 
         if not os.path.isfile(hooks_filepath):
             raise FileNotFoundError(f'File "{hooks_filepath}" not exists')
 
-    def load_hooks(self) -> dict:
+    def _load_hooks(self) -> dict:
         with open(self._hooks_filepath, 'r') as reader:
             hooks = reader.load(reader)
 
-        self._hooks = hooks
         return hooks
 
     def execute_hook(self, action: str) -> int:
@@ -37,7 +35,7 @@ class Hooks(object):
         :rtype: int
         """
 
-        for hook in self._hooks:
+        for hook in self._load_hooks():
             if hook['action'] == action:
                 script = hook['script']
                 return os.system(script)
