1fde0b68157c45aadbb2f72f83410db6622c8beb
Jaedson Silva <contacanal798@gmail.com>
2022-10-15 18:28:55 -0300

    checking if hooks file exists

--- pie/hooks.py
+++ pie/hooks.py
@@ -5,3 +5,6 @@ import json
 class Hooks(object):
     def __init__(self, hooks_filepath: str) -> None:
         self._hooks_filepath = hooks_filepath
+
+        if not os.path.isfile(hooks_filepath):
+            raise FileNotFoundError(f'File "{hooks_filepath}" not exists')
