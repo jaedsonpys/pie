331a9e9b990411e480cb3156a67eb4e58fd5f1ab
Jaedson Silva <contacanal798@gmail.com>
2022-10-15 18:30:59 -0300

    adding load_hooks method in Hooks class

--- pie/hooks.py
+++ pie/hooks.py
@@ -5,6 +5,14 @@ import json
 class Hooks(object):
     def __init__(self, hooks_filepath: str) -> None:
         self._hooks_filepath = hooks_filepath
+        self.hooks = {}
 
         if not os.path.isfile(hooks_filepath):
             raise FileNotFoundError(f'File "{hooks_filepath}" not exists')
+
+    def load_hooks(self) -> dict:
+        with open(self._hooks_filepath, 'r') as reader:
+            hooks = reader.load(reader)
+
+        self._hooks = hooks
+        return hooks
