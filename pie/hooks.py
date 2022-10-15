29c114d294c073373898599473644ab69a4176b6
Jaedson Silva <contacanal798@gmail.com>
2022-10-15 18:39:40 -0300

    adding docstring to Hooks methods

--- pie/hooks.py
+++ pie/hooks.py
@@ -4,6 +4,17 @@ import json
 
 class Hooks(object):
     def __init__(self, hooks_filepath: str) -> None:
+        """Create a new instance of the Hook class.
+
+        This class is responsible for loading and executing
+        specific action hooks such as `commit`, `add` and
+        other commands.
+
+        :param hooks_filepath: Hooks file path.
+        :type hooks_filepath: str
+        :raises FileNotFoundError: If hooks file is not found.
+        """
+
         self._hooks_filepath = hooks_filepath
         self.hooks = {}
 
@@ -18,6 +29,14 @@ class Hooks(object):
         return hooks
 
     def execute_hook(self, action: str) -> int:
+        """Run the specified action hook script.
+
+        :param action: Hook action
+        :type action: str
+        :return: Script status code.
+        :rtype: int
+        """
+
         for hook in self._hooks:
             if hook['action'] == action:
                 script = hook['script']
