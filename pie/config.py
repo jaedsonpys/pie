d64399375ded4e11f0e8d0637b2011b63d7a1dcd
Jaedson Silva <contacanal798@gmail.com>
2022-10-16 00:29:17 -0300

    receiving author and author_email arguments instead of a dictionary

--- pie/config.py
+++ pie/config.py
@@ -8,22 +8,22 @@ class PieConfig(object):
         self._config_dirpath = os.path.join(self._home_dirpath, '.pie-config')
         self._config_author_filepath = os.path.join(self._config_dirpath, 'author.json')
 
-        author_info = {
-            'author': None,
-            'author_email': None
-        }
-
         if not os.path.isdir(self._pie_config_dirpath):
             os.mkdir(self._pie_config_dirpath)
-            self.write_author_info(author_info)  
+            self.write_author_info(author=None, author_email=None)  
         else:
             if not os.path.isfile(self._config_author_filepath):
-                self.write_author_info(author_info)
+                self.write_author_info(author=None, author_email=None)  
 
-    def write_author_info(self, author_info: dict) -> None:
+    def write_author_info(self, author: str, author_email: str) -> None:
+        author_info = {
+            'author': author,
+            'author_email': author_email
+        }
+
         with open(self._config_author_filepath, 'w') as writer:
             json.dump(author_info, writer, indent=4)
-    
+
     def get_author_info(self) -> dict:
         with open(self._config_author_filepath, 'r') as reader:
             author = json.load(reader)
